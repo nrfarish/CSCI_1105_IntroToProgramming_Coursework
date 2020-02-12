9da93bf06b9fb2d2338a9e5fa5c738e96fb1bccc
nrfarish <60949979+nrfarish@users.noreply.github.com>
2020-02-12 12:01:44 -0700

    Update ComputeExpression.java
    Added descriptive comment

--- Chapter 1/ComputeExpression.java
+++ Chapter 1/ComputeExpression.java
@@ -1,3 +1,6 @@
+/* Author Nic Farish
+Code to compute a mathmatical expression */
+
 public class ComputeExpression {
 	public static void main(String[] args) {
 		System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
