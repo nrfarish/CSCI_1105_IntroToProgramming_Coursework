3d6338f0f3e86dace6e17142fb56c5ef0714f61f
nrfarish <60949979+nrfarish@users.noreply.github.com>
2020-02-12 12:05:39 -0700

    Update Exercise113.java
    Added comment.

--- Chapter 1/Exercise113.java
+++ Chapter 1/Exercise113.java
@@ -1,3 +1,9 @@
+/*
+Nic Farish
+2/12/20
+Program to solve a specific mathmatical equation.
+*/
+
 class Exercise113 {
 	public static void main(String[] args) {
 		System.out.println((44.5 * 0.55 - 50.2 * 5.9) / (3.4 * 0.55 - 50.2 * 2.1));
