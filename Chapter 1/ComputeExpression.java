8d5ce782d8a81a40ce2a95745fafa8fe43d0d62b
nrfarish <60949979+nrfarish@users.noreply.github.com>
2020-02-12 17:17:13 -0700

    Add files via upload
    Added the correct files to Chapter 1 folder

---
+++ Chapter 1/ComputeExpression.java
@@ -0,0 +1,12 @@
+
+/* 
+Author Nic Farish
+2/12/20
+Code to compute a mathmatical expression
+*/
+
+public class ComputeExpression {
+	public static void main(String[] args) {
+		System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+	}
+}                  
---
+++ Chapter 1/LinearEquation.java
@@ -0,0 +1,12 @@
+ /*
+Nic Farish
+2/12/20
+Program to solve a specific mathmatical equation.
+*/
+
+class Exercise113 {
+	public static void main(String[] args) {
+		System.out.println((44.5 * 0.55 - 50.2 * 5.9) / (3.4 * 0.55 - 50.2 * 2.1));
+		System.out.println((3.4 * 5.9 - 44.5 * 2.1) / (3.4 * 0.55 - 50.2 * 2.1));
+	}
+}
